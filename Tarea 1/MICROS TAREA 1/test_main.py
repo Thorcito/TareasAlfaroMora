24918962aa6fe3343cbd30f1c1b35703addef9b9
Rastmoag <randy.mora.aguero@estudiantec.cr>
2022-02-18 02:04:41 -0600

    Se creó test_main.py y se definieron las funciones de prueba

--- Tarea 1/MICROS TAREA 1/test_main.py
+++ Tarea 1/MICROS TAREA 1/test_main.py
@@ -3,6 +3,37 @@
 from main import *  #se importa lo del main para no tener que definir de nuevo las funciones
 
 
+def test_cambio_letras():
+    x="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    assert string_work(x)=="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-entrada = str(input("Inserte mensaje con letras")) #se le solicita al usuario que inserte un string y este se le asigna a una variable
-string_work(entrada) # se hace un llamado a la funcion string_work
+def test_entrada_numero():
+    x="21"
+    assert string_work(x)==1
+
+def test_entrada_letrasynumeros():
+    x="ASjsd21"
+    assert string_work(x)==2
+
+#def test_0_a_99():
+    #x=0
+    #while x<100:
+       # y=x;
+       # x=x+1;
+    #assert num_to_str(y)==letras_fin
+
+def test_0_a_99():
+    x="68"
+    assert num_to_str(x)=="sesenta_y_ocho"
+
+def test_string():
+    x="AlfaroMora"
+    assert num_to_str(x)==4
+
+def test_neg_dec_may():
+    x="-20"
+    y="123"
+    z="3.2"
+    assert num_to_str(x) == 3
+    assert num_to_str(y) == 3
+    assert num_to_str(z) == 3
