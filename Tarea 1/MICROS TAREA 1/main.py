50b011c1ad1037b02d4f38cae49a54d6b38b2fa9
Rastmoag <randy.mora.aguero@estudiantec.cr>
2022-02-18 11:41:45 -0600

    Se corrigieron los errores por medio de flake8

--- Tarea 1/MICROS TAREA 1/main.py
+++ Tarea 1/MICROS TAREA 1/main.py
@@ -1,41 +1,45 @@
+# Se importan librerías
+from num2words import num2words
+
 # Este es el algoritmop para el metodo string_work
 # Chequea si un string contiene solo letras sino retorna un codigo de error
 
-def string_work(entrada):# se crea la funcion
-    if entrada.isalpha(): # se crea un condicional en el que se compara si la entrada es un caracter de la A a las Z
-        entradilla = entrada.swapcase() # en caso de ser TRUE, se alternan las letras mayusculas con las minusculas
-        print(entradilla) # da como resultado la entrada alternada
-        return entradilla
-    if entrada.isnumeric():
-        print("1")
-        return 1
-    else: # en caso de no ser FASE
-        print ("2") #imprime codigo de error unico, el 1 se imprime cunado el texto ingresado contiene numeros
-        return 2 # se finaliza la funcion
 
-# Este es el algoritmo para el metodo num_to_str
-# verifica que el string entrado contenga solo numeros y de no serlo, retorna un codigo
-# de error unico, en caso de ser verdadero traduce el numero a texto
-from num2words import num2words #se importan las librerias a utilizar
+def string_work(entrada):  # Se crea la funcion
+    # se compara si entrada es un caracter de la A a las Z
+    if entrada.isalpha():
+        entradilla = entrada.swapcase()  # if True, se cambian MAY por min
+        print(entradilla)   # Imprime resultado
+        return entradilla  # Retorna resultado
+    if entrada.isnumeric():  # Verifica si es número
+        print("1")  # Imprime código de error: 1
+        return 1  # Retorna error:1
+    else:  # En caso de ser alfanumérico
+        print("2")  # Imprime error único 2
+        return 2  # Retorna error:2
 
-def num_to_str(entry): #se crea la funcion
-    if entry.isnumeric(): #se crea un condicional en el que se verifica si el entry es un caracter del 0 al 9
-        ent=int(entry)
-        if ent <= 99:
-            letras_esp = num2words(ent, lang='es') #si la condicion se cumple, convierte el numero a letras
-            letras_fin = letras_esp.replace(" ","_"); #la libreria divide las palabras con un espacio, entonces se cambia el espacio con el simbolo _
-            print(letras_fin) #se imprime el resultado del numero en letras
+
+# Se define la función
+def num_to_str(entry):
+    if entry.isnumeric():  # Se verifica si el entry es un caracter del 0 al 9
+        ent = int(entry)  # Define la entrada como int
+        if ent <= 99:  #
+            # si la condicion se cumple, convierte el numero a letras
+            letras_esp = num2words(ent, lang='es')
+            # la libreria divide las palabras con un espacio
+            # se cambia el espacio con el simbolo _
+            letras_fin = letras_esp.replace(" ", "_")
+            print(letras_fin)  # se imprime el resultado del numero en letras
             return letras_fin
         elif ent > 99:
             print("3")
             return 3
-    else: #en caso de que pase un string
+    else:  # en caso de que pase un string
         if entry.isalpha():
-            print ("4")#imprime codigo de error unico, el 2 se imprime cunado el texto ingresado contiene letras
-            return 4 #finaliza la funcion
+            # imprime codigo de error unico, el 2 se imprime cunado el texto
+            # ingresado contiene letras
+            print("4")
+            return 4  # finaliza la funcion
         else:
             print("3")
             return 3
-
-
-
--- Tarea 1/MICROS TAREA 1/test_main.py
+++ Tarea 1/MICROS TAREA 1/test_main.py
@@ -1,39 +1,47 @@
-#este es el archivo para hacer la pruebas de pytest
+# este es el archivo para hacer la pruebas de pytest
 
-from main import *  #se importa lo del main para no tener que definir de nuevo las funciones
+# se importa lo del main para no tener que definir de nuevo las funciones
+from main import string_work
+from main import num_to_str
 
 
 def test_cambio_letras():
-    x="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    assert string_work(x)=="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    assert string_work(
+        x) == "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
 
 def test_entrada_numero():
-    x="21"
-    assert string_work(x)==1
+    x = "21"
+    assert string_work(x) == 1
+
 
 def test_entrada_letrasynumeros():
-    x="ASjsd21"
-    assert string_work(x)==2
+    x = "ASjsd21"
+    assert string_work(x) == 2
 
-#def test_0_a_99():
-    #x=0
-    #while x<100:
-       # y=x;
-       # x=x+1;
-    #assert num_to_str(y)==letras_fin
+# def test_0_a_99():
+    # x=0
+    # while x<100:
+    # y=x;
+    # x=x+1;
+    # assert num_to_str(y)==letras_fin
+
 
 def test_0_a_99():
-    x="68"
-    assert num_to_str(x)=="sesenta_y_ocho"
+    x = "68"
+    assert num_to_str(x) == "sesenta_y_ocho"
+
 
 def test_string():
-    x="AlfaroMora"
-    assert num_to_str(x)==4
+    x = "AlfaroMora"
+    assert num_to_str(x) == 4
+
 
 def test_neg_dec_may():
-    x="-20"
-    y="123"
-    z="3.2"
+    x = "-20"
+    y = "123"
+    z = "3.2"
     assert num_to_str(x) == 3
     assert num_to_str(y) == 3
     assert num_to_str(z) == 3
